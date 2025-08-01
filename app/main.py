c8a5c7d864b7e2132aeb2f170b9e2d447fb5f371
dhruv <dhruvkishnani@gmail.com>
2025-07-31 22:30:43 -0700

    initial commit

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from app.routers import claims, users, images, detected_part, detected_damage
+from app.routers import claims, users, images, detected_part, detected_damage, cost_estimates, fraud_flags
 app = FastAPI()
 
 app.include_router(claims.router)
@@ -7,3 +7,5 @@ app.include_router(users.router)
 app.include_router(images.router)
 app.include_router(detected_part.router)
 app.include_router(detected_damage.router)
+app.include_router(cost_estimates.router)
+app.include_router(fraud_flags.router)
